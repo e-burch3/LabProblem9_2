aa78d1b4bca947025a3fc0fbb3b4335913d02b57
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2023-10-26 12:44:31 -0400

    added hasAdjacentDuplicates function.

--- LabProblem9_2.cpp
+++ LabProblem9_2.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 bool isSortedIncreasing(int array[], int size);
 bool isSortedDecreasing(int array[], int size);
+bool hasAdjecentDuplicates(int array[], int size);
+
 
 int main(void) {
 	const int SIZE = 6;
@@ -31,14 +33,30 @@ int main(void) {
 		cout << "The data are decreasing\n";
 	else
 		cout << "The data are not decreasing\n";
+	if (hasAdjecentDuplicates(numbers, SIZE))
+		cout << "The data has adjacent duplicates\n";
+	else
+		cout << "The data does not have adjacent duplicates\n";
 	return 0;
+}
+
+bool hasAdjecentDuplicates(int array[], int size) {
+	for (int i = 1; i < size; i++) {
+		if (array[i] == array[i - 1] || array[i] == array[i + 1])
+			return true;
+		else if (i == size - 1)
+			break;
+		else
+			continue;
+	}
+	return false;
 }
 
 bool isSortedDecreasing(int array[], int size) {
 	for (int i = size - 1; i >= 0; i--) {
 		if (i == 0)
 			break;
-		else if (array[i] < array[i - 1])
+		else if (array[i] <= array[i - 1])
 			continue;
 		else
 			return false;
@@ -50,7 +68,7 @@ bool isSortedIncreasing(int array[], int size) {
 	for (int i = 0; i < size; i++) {
 		if (i == size - 1)
 			break;
-		else if (array[i] < array[i + 1]) 
+		else if (array[i] <= array[i + 1]) 
 			continue;
 		else 
 			return false;
